d87ef890d58cd37160c85b577fcd815d2a1842da
TomasSilvetti <tomas.silvetti@alu.frt.utn.edu.ar>
2026-04-13 18:57:01 -0300

    fix 3 symbols

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,11 +31,7 @@ export default function RootLayout({
       lang="en"
       className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
     >
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=block" />
-      </head>
+      <head />
       <body className="min-h-full flex flex-col">
         <AuthSessionProvider>{children}</AuthSessionProvider>
       </body>
